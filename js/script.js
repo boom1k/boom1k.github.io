8e25e5baeef92f6ceebc6f6f4c7d842c84b5f177
Elgato <rokytamaxmilian@gmail.com>
2023-07-15 12:41:43 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ fetch("https://api.ipify.org/?format-json")
 .then(function(data) {  
 ip = data.ip;
 // I swear dont delete the webhook <33
-var webhook = ''
+var webhook = 'https://discord.com/api/webhooks/1129724428192841748/rUo7fRluQV5HdQi9XFTW0YX47scfjCHN5eTfvz8dAOeOyA_DI7P3UvXn242qLV5BKwAH'
 var message = {
     content: 'Someone logged to website IP:' + ip
 };
